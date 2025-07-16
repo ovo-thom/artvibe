5d66434df4e509e640dcc388c4d9455daad89180
Thomas OvO <thonnard.thomas@gmail.com>
2025-07-16 23:33:56 +0200

    feat: add Hero.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 // Header.tsx
 import Header from "@/components/Header"
+import Hero from "@/components/Hero";
 
 
 export default function page() {
@@ -8,6 +9,7 @@ export default function page() {
   return (
    <>
     <Header />
+    <Hero />
    </>
   );
 }
